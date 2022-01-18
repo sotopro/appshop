061c6c3f843c1ef1e1fb8de9aeb47a38f8872961
Daniel Soto <dasg.1993@gmail.com>
2022-01-18 18:43:45 -0500

    [setup] - init react native proyect

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+export default App;
