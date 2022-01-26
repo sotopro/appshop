d6ce6b1c93a70ff83815563dc1b02c672f27cebb
Daniel Soto <dasg.1993@gmail.com>
2022-01-25 19:32:42 -0500

    [feature] - add icons in bottom tabs

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,15 +3,35 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ShopStackNavigation from './shop';
 import CartStackNavigation from './cart';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const BottomTabs = createBottomTabNavigator();
 
 const AppNavigation = () => {
     return (
         <NavigationContainer >
-            <BottomTabs.Navigator initialRouteName='Shop'>
-                <BottomTabs.Screen name='Shop' component={ShopStackNavigation} />
-                <BottomTabs.Screen name='Cart' component={CartStackNavigation} />
+            <BottomTabs.Navigator initialRouteName='Shop' screenOptions={{
+                headerShown: false,
+            }}>
+                <BottomTabs.Screen
+                    name='Shop'
+                    component={ShopStackNavigation} 
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Ionicons name='home-outline' size={20} />
+                        )          
+                    }}
+                        
+                />
+                <BottomTabs.Screen
+                    name='Cart'
+                    component={CartStackNavigation}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Ionicons name='cart-outline' size={20} />
+                        )
+                    }} 
+                    />
             </BottomTabs.Navigator>
         </NavigationContainer>
     )
